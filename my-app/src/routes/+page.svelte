ee472aa2ea22cf627818e2c7601a36f09bb9a47b
John Does it <hello@johndoesit.be>
2025-09-26 12:44:58 +0200

    Update +page.svelte

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -402,23 +402,23 @@
     <!-- svelte-ignore a11y_no_static_element_interactions -->
     <!-- svelte-ignore a11y_click_events_have_key_events -->
     <gallery-container bind:this={galleryContainer} onclick={(event) => reorderImage(event)}>
-      <img src="images/nestor-1.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-2.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-3.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-4.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-5.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-6.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-7.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-8.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-9.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-10.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-11.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-12.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-13.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-14.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-15.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-16.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
-      <img src="images/nestor-17.jpg" alt="Nestor Schoonbroodt" loading="lazy" />
+      <img src="images/nestor-1.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-2.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-3.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-4.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-5.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-6.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-7.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-8.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-9.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-10.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-11.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-12.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-13.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-14.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-15.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-16.jpg" alt="Nestor Schoonbroodt" />
+      <img src="images/nestor-17.jpg" alt="Nestor Schoonbroodt" />
     </gallery-container>
     <button onclick={() => showNextImageInQueue()}>Photo suivante</button>
   </pictures-container>
