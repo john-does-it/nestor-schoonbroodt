0780c0135ddc7e2aa8664edef5d325952e15d5f8
John Does it <hello@johndoesit.be>
2025-09-30 14:36:08 +0200

    Update link titles for improved accessibility
    Revised the title attributes of credit section links to provide more descriptive and accessible information for users.

--- my-app/src/routes/credits/+page.svelte
+++ my-app/src/routes/credits/+page.svelte
@@ -2,13 +2,13 @@
   <h1>Crédits</h1>
   <p>
     Site web par
-    <a href="https://www.johndoesit.be/" title="John Does it">John Does it</a>
+    <a href="https://www.johndoesit.be/" title="Accéder au site web de John Does it">John Does it</a>
   </p>
   <p>
     Vidéo par 
-    <a href="https://pixabay.com/users/doktorkleinmusic-5871510/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Norberto Navarro Valiente">Norberto Navarro Valiente</a>
+    <a href="https://pixabay.com/users/doktorkleinmusic-5871510/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Accéder au profil de Norberto Navarro Valiente sur Pixabay">Norberto Navarro Valiente</a>
     venant de
-    <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Pixabay">Pixabay</a>
+    <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Accéder à Pixabay">Pixabay</a>
   </p>
 </credits-container>
 
