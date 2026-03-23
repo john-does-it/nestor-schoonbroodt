c6aeb5ad7dc9d5f5cead1b8432ad3b37475bfbe0
john-does-it <hello@johndoesit.be>
2026-03-23 20:55:31 +0100

    Update +page.svelte

--- my-app/src/routes/credits/+page.svelte
+++ my-app/src/routes/credits/+page.svelte
@@ -1,14 +1,17 @@
 <credits-container>
   <h1>Crédits</h1>
   <p>
-    Site web par
-    <a href="https://www.johndoesit.be/" title="Accéder au site web de John Does it">John Does it</a>
-  </p>
-  <p>
-    Vidéo par 
-    <a href="https://pixabay.com/users/doktorkleinmusic-5871510/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Accéder au profil de Norberto Navarro Valiente sur Pixabay">Norberto Navarro Valiente</a>
+    Vidéo par
+    <a
+      href="https://pixabay.com/users/doktorkleinmusic-5871510/?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241"
+      title="Accéder au profil de Norberto Navarro Valiente sur Pixabay"
+      >Norberto Navarro Valiente</a
+    >
     venant de
-    <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241" title="Accéder à Pixabay">Pixabay</a>
+    <a
+      href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=video&utm_content=301241"
+      title="Accéder à Pixabay">Pixabay</a
+    >
   </p>
 </credits-container>
 
@@ -20,6 +23,6 @@
 
     h1 {
       margin-top: 0;
-     }
+    }
   }
 </style>
